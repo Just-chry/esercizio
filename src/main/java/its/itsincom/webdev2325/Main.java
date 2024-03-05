0d23b4c5b572389cbbcf317279b04eac1a930d04
JustChry <jia.chen@itsincom.it>
2024-03-05 12:12:53 +0100

    Inizio
    :

---
+++ src/main/java/its/itsincom/webdev2325/Main.java
@@ -0,0 +1,17 @@
+package its.itsincom.webdev2325;
+
+/**
+ * Hello world!
+ */
+public final class Main {
+    private Main() {
+    }
+
+    /**
+     * Says hello to the world.
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
